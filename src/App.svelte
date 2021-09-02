37e34fde3824f801b63bd0cb2917d73e5ef4d525
jensengbg-jack-carling <72305598+jensengbg-jack-carling@users.noreply.github.com>
2021-09-02 17:24:56 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>Chocolatier</h1>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+  main {
+    width: 100vw;
+    height: 100vh;
+    padding: 1rem;
+    margin: 0;
+  }
+  h1 {
+    color: #ff3e00;
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+  }
+</style>
